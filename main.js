86358dfbb86c07fe3b988357545e53f4fc5bb217
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2020-08-17 15:31:33 +0100

    Added some basic ball animation

--- main.js
+++ main.js
@@ -4,10 +4,100 @@ const ymax=720;
 
 // Game state is global to prevent it going out of scope
 var gs={
+  // Animation frame of reference
+  step:(1/60), // target step time @ 60 fps
+  acc:0, // accumulated time since last frame
+  lasttime:0, // time of last frame
+
+  // Canvas object
   canvas:null,
-  ctx:null
+  ctx:null,
+
+  ball:{
+    // Position
+    x:0,
+    y:0,
+    z:0,
+
+    // Velocity
+    vx:4,
+    vy:4,
+    vz:0
+  },
+
+  state:0 // 0=intro, 1=title, 2=ingame 3=completed
 };
 
+// Check if the ball is currently moving
+function ballmoving()
+{
+  return ((gs.ball.vx!=0) || (gs.ball.vy!=0) || (gs.ball.vz!=0));
+}
+
+// Move the ball onwards
+function moveball()
+{
+  gs.ball.x+=gs.ball.vx;
+  gs.ball.y+=gs.ball.vy;
+  gs.ball.z+=gs.ball.vz;
+
+  // Stop it going off screen
+  if (gs.ball.x>xmax) gs.ball.x=0;
+  if (gs.ball.y>ymax) gs.ball.y=0;
+}
+
+// Render the current scene
+function render()
+{
+  // Clear the screen
+  gs.ctx.clearRect(0, 0, gs.canvas.width, gs.canvas.height);
+
+  gs.ctx.beginPath();
+  gs.ctx.arc(gs.ball.x, gs.ball.y, 10, 0, 2*Math.PI);
+  gs.ctx.fill();
+}
+
+// Update step
+function update()
+{
+  // Move the ball
+  moveball();
+}
+
+// Called once per frame for animation updates
+function rafcallback(timestamp)
+{
+  // First time round, just save epoch
+  if (gs.lasttime>0)
+  {
+    // Determine accumulated time since last call
+    gs.acc+=((timestamp-gs.lasttime) / 1000);
+
+    // If it's more than 15 seconds since last call, reset
+    if ((gs.acc>gs.step) && ((gs.acc/gs.step)>(60*15)))
+      gs.acc=gs.step*2;
+
+    // Process the "steps" since last call
+    while (gs.acc>gs.step)
+    {
+      update();
+
+      gs.acc-=gs.step;
+    }
+  }
+
+  // Remember when we were last called
+  gs.lasttime=timestamp;
+
+  // Perform a render step
+  render();
+
+  // Request we are called on next frame if still playing
+  if (gs.state==2)
+    window.requestAnimationFrame(rafcallback);
+}
+
+// Handle screen resizing to maintain correctly centered display
 function resize()
 {
   var height=window.innerHeight;
@@ -45,6 +135,15 @@ function startup()
 
   gs.canvas=document.getElementById('canvas');
   gs.ctx=this.canvas.getContext('2d');
+
+  gs.ctx.fillStyle="rgba(255,255,255,1)";
+  gs.ctx.strokeStyle="rgba(255,255,255,1)";
+  gs.ctx.lineWidth=1;
+
+  // Put straight into game
+  gs.state=2;
+
+  window.requestAnimationFrame(rafcallback);
 
   resize();
   window.addEventListener("resize", resize);
